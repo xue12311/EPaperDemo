e8ed492afd768edf6b8a221103178f0514a84948
xue12311 <1712530035@qq.com>
2022-08-08 22:08:35 +0800

    定位权限 获取

--- library_epager/src/main/java/com/ayxls/library_epager/activity/EspTouch2Activity.kt
+++ library_epager/src/main/java/com/ayxls/library_epager/activity/EspTouch2Activity.kt
@@ -1,13 +1,15 @@
 package com.ayxls.library_epager.activity
 
+import android.content.pm.PackageManager
 import android.net.wifi.WifiManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.ayxls.library_epager.R
 import com.ayxls.library_epager.constant.ARouterConstants
 import com.ayxls.library_epager.databinding.ActivityEsptouch2Binding
-import com.ayxls.library_epager.ext.requestPermission
 import com.ayxls.library_epager.ext.showPermissionDialog
 import com.ayxls.library_epager.viewmodel.EspTouch2ViewModel
 import com.blankj.utilcode.util.PermissionUtils
@@ -32,46 +34,100 @@ class EspTouch2Activity : BaseVmVbActivity<EspTouch2ViewModel, ActivityEsptouch2
 
     override fun initData() {
         super.initData()
+        //获取权限
         onCheckLocationPermission()
     }
 
+    /**
+     * 申请权限
+     */
     private fun onCheckLocationPermission() {
-        requestPermission(mViewModel.getAccessFineLocationPermission(), {
-            showToastMessage("获取权限成功")
-        }, {
-            showPermissionDialog(
-                getString(R.string.text_dialog_location_permission_title),
-                getString(R.string.text_dialog_location_permission_message),
-                getString(R.string.text_dialog_permission_authorize), {
-                    PermissionUtils.permission(it)
-                        .callback(object : PermissionUtils.FullCallback {
-                            override fun onGranted(granted: MutableList<String>) {
-                                showToastMessage("获取权限成功")
-                            }
-                            override fun onDenied(
-                                deniedForever: MutableList<String>,
-                                denied: MutableList<String>
-                            ) {
-                                if (deniedForever.isNotEmpty()) {
-                                    showToastMessage("获取权限失败 不再询问")
-                                } else {
-                                    showToastMessage("获取权限失败")
-                                }
-                            }
-                        }).request()
-                }, {
-                    showToastMessage("获取权限失败")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //判断是否已经授权
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    mViewModel.getAccessFineLocationPermission()
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {//已经授权
+                onLocationPermissionGranted()
+            } else {//申请授权
+                requestLocationPermission.launch(mViewModel.getAccessFineLocationPermission())
+            }
+        } else {
+            onLocationPermissionGranted()
+        }
+    }
+
+
+    private val requestLocationPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            //权限获取成功
+            if (isGranted) {
+                onLocationPermissionGranted()
+            } else { //权限获取失败
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    //是否已经勾选 禁止后不再询问
+                    val isDenied = shouldShowRequestPermissionRationale(mViewModel.getAccessFineLocationPermission())
+                    if (isDenied) {
+                        //被拒接 且 没勾选不再询问
+                        showLocationPermissionDialog()
+                    } else {
+                        //被拒接 且 勾选了不再询问
+                        showLocationPermissionSettingDialog()
+                    }
+                } else {
+                    onLocationPermissionGranted()
                 }
-            )
-        }, {
-            showPermissionDialog(
-                getString(R.string.text_dialog_location_permission_title),
-                getString(R.string.text_dialog_location_permission_message2),
-                getString(R.string.text_dialog_permission_setting), {
-                    PermissionUtils.launchAppDetailsSettings()
-                }, {
-                    showToastMessage("获取权限失败 不再询问")
-                })
-        })
+            }
+        }
+
+    /**
+     * 权限申请成功
+     */
+    private fun onLocationPermissionGranted() {
+        showToastMessage("获取权限成功")
+    }
+
+    /**
+     * 被拒绝且未勾选不再询问
+     */
+    private fun onLocationPermissionDenied() {
+        showToastMessage("获取权限失败")
+    }
+
+    /**
+     * 被拒绝且勾选不再询问
+     */
+    private fun onLocationPermissionExplained() {
+        showToastMessage("获取权限失败 不再询问")
+    }
+
+    /**
+     * 位置权限 提示框 - 未勾选不再询问
+     */
+    private fun showLocationPermissionDialog() {
+        showPermissionDialog(
+            getString(R.string.text_dialog_location_permission_title),
+            getString(R.string.text_dialog_location_permission_message),
+            getString(R.string.text_dialog_permission_authorize), {
+                requestLocationPermission.launch(mViewModel.getAccessFineLocationPermission())
+            }, {
+                onLocationPermissionDenied()
+            }
+        )
+    }
+
+    /**
+     * 位置权限 提示框 - 勾选了不再询问
+     */
+    private fun showLocationPermissionSettingDialog() {
+        showPermissionDialog(
+            getString(R.string.text_dialog_location_permission_title),
+            getString(R.string.text_dialog_location_permission_message2),
+            getString(R.string.text_dialog_permission_setting), {
+                PermissionUtils.launchAppDetailsSettings()
+            }, {
+                onLocationPermissionExplained()
+            })
     }
 }
--- library_epager/src/main/java/com/ayxls/library_epager/ext/ActivityExt.kt
+++ library_epager/src/main/java/com/ayxls/library_epager/ext/ActivityExt.kt
@@ -4,7 +4,9 @@ import android.app.Activity
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.ayxls.library_epager.R
@@ -31,78 +33,79 @@ fun Activity.showPermissionDialog(
 }
 
 
-/**
- * 单个权限 获取
- * @param mPermission
- * @param onPermissionGranted 权限申请成功
- * @param onPermissionDenied 被拒绝且未勾选不再询问
- * @param onPermissionExplained 被拒绝且勾选不再询问
- */
-fun ComponentActivity.requestPermission(
-    mPermission: String,
-    onPermissionGranted: ((String) -> Unit)? = null,
-    onPermissionDenied: ((String) -> Unit)? = null,
-    onPermissionExplained: ((String) -> Unit)? = null
-) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        //判断是否已经授权
-        if (ContextCompat.checkSelfPermission(
-                this,
-                mPermission
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            onPermissionGranted?.invoke(mPermission)
-        } else {
-            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-                //权限获取成功
-                if (isGranted) {
-                    onPermissionGranted?.invoke(mPermission)
-                } else { //权限获取失败
-                    //是否已经勾选 禁止后不再询问
-                    val isDenied = shouldShowRequestPermissionRationale(mPermission)
-                    //被拒接 且 没勾选不再询问
-                    if (isDenied) {
-                        onPermissionDenied?.invoke(mPermission)
-                        //被拒接 且 勾选了不再询问
-                    } else {
-                        onPermissionExplained?.invoke(mPermission)
-                    }
-                }
-            }.launch(mPermission)
-        }
-    } else {
-        onPermissionGranted?.invoke(mPermission)
-    }
-}
+//
+///**
+// * 单个权限 获取
+// * @param mPermission
+// * @param onPermissionGranted 权限申请成功
+// * @param onPermissionDenied 被拒绝且未勾选不再询问
+// * @param onPermissionExplained 被拒绝且勾选不再询问
+// */
+//fun ComponentActivity.requestPermission(
+//    mPermission: String,
+//    onPermissionGranted: ((String) -> Unit)? = null,
+//    onPermissionDenied: ((String) -> Unit)? = null,
+//    onPermissionExplained: ((String) -> Unit)? = null
+//) {
+//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//        //判断是否已经授权
+//        if (ContextCompat.checkSelfPermission(
+//                this,
+//                mPermission
+//            ) == PackageManager.PERMISSION_GRANTED
+//        ) {
+//            onPermissionGranted?.invoke(mPermission)
+//        } else {
+//            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+//                //权限获取成功
+//                if (isGranted) {
+//                    onPermissionGranted?.invoke(mPermission)
+//                } else { //权限获取失败
+//                    //是否已经勾选 禁止后不再询问
+//                    val isDenied = shouldShowRequestPermissionRationale(mPermission)
+//                    //被拒接 且 没勾选不再询问
+//                    if (isDenied) {
+//                        onPermissionDenied?.invoke(mPermission)
+//                        //被拒接 且 勾选了不再询问
+//                    } else {
+//                        onPermissionExplained?.invoke(mPermission)
+//                    }
+//                }
+//            }.launch(mPermission)
+//        }
+//    } else {
+//        onPermissionGranted?.invoke(mPermission)
+//    }
+//}
 
-fun Fragment.showPermissionDialog(
-    strTitle: String,
-    strMessage: String,
-    strConfirmText: String? = getString(R.string.esptouch2_confirm),
-    onConfirm: (() -> Unit)? = null,
-    onCancel: (() -> Unit)? = null
-) {
-    activity?.showPermissionDialog(strTitle, strMessage, strConfirmText, onConfirm, onCancel)
-}
+//fun Fragment.showPermissionDialog(
+//    strTitle: String,
+//    strMessage: String,
+//    strConfirmText: String? = getString(R.string.esptouch2_confirm),
+//    onConfirm: (() -> Unit)? = null,
+//    onCancel: (() -> Unit)? = null
+//) {
+//    activity?.showPermissionDialog(strTitle, strMessage, strConfirmText, onConfirm, onCancel)
+//}
 
 
-/**
- * 单个权限 获取
- * @param mPermission
- * @param onPermissionGranted 权限申请成功
- * @param onPermissionDenied 被拒绝且未勾选不再询问
- * @param onPermissionExplained 被拒绝且勾选不再询问
- */
-fun Fragment.requestPermission(
-    mPermission: String,
-    onPermissionGranted: ((String) -> Unit)? = null,
-    onPermissionDenied: ((String) -> Unit)? = null,
-    onPermissionExplained: ((String) -> Unit)? = null
-) {
-    activity?.requestPermission(
-        mPermission,
-        onPermissionGranted,
-        onPermissionDenied,
-        onPermissionExplained
-    )
-}
+///**
+// * 单个权限 获取
+// * @param mPermission
+// * @param onPermissionGranted 权限申请成功
+// * @param onPermissionDenied 被拒绝且未勾选不再询问
+// * @param onPermissionExplained 被拒绝且勾选不再询问
+// */
+//fun Fragment.requestPermission(
+//    mPermission: String,
+//    onPermissionGranted: ((String) -> Unit)? = null,
+//    onPermissionDenied: ((String) -> Unit)? = null,
+//    onPermissionExplained: ((String) -> Unit)? = null
+//) {
+//    activity?.requestPermission(
+//        mPermission,
+//        onPermissionGranted,
+//        onPermissionDenied,
+//        onPermissionExplained
+//    )
+//}
