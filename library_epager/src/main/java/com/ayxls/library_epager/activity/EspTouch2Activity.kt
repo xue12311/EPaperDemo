59853f36b7cdaf361daad99f8b8db5097b44da37
xue12311 <1712530035@qq.com>
2022-08-15 22:36:21 +0800

    获取 wifi信息

--- library_epager/src/main/java/com/ayxls/library_epager/activity/EspTouch2Activity.kt
+++ library_epager/src/main/java/com/ayxls/library_epager/activity/EspTouch2Activity.kt
@@ -1,24 +1,46 @@
 package com.ayxls.library_epager.activity
 
+import android.content.Context
 import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.ayxls.library_epager.R
 import com.ayxls.library_epager.constant.ARouterConstants
 import com.ayxls.library_epager.databinding.ActivityEsptouch2Binding
 import com.ayxls.library_epager.ext.showPermissionDialog
 import com.ayxls.library_epager.viewmodel.EspTouch2ViewModel
+import com.blankj.utilcode.util.LogUtils
 import com.blankj.utilcode.util.PermissionUtils
 import com.xue.base_common_library.base.activity.BaseVmVbActivity
 
 @Route(path = ARouterConstants.ARouterActivityEspTouch2)
 class EspTouch2Activity : BaseVmVbActivity<EspTouch2ViewModel, ActivityEsptouch2Binding>() {
     //wifi 管理器
-    private val mWifiManager by lazy { getApplicationContext().getSystemService(WIFI_SERVICE) as WifiManager }
+    private val mWifiManager by lazy { applicationContext.getSystemService(WIFI_SERVICE) as WifiManager }
+    private val mConnectivityManager by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            (getSystemService(ConnectivityManager::class.java)) as ConnectivityManager
+        } else {
+            (getSystemService(Context.CONNECTIVITY_SERVICE)) as ConnectivityManager
+        }
+    }
+    private val mNetworkRequest by lazy {
+        NetworkRequest
+            .Builder()
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .build()
+    }
 
     override fun initView(savedInstanceState: Bundle?) {
         super.initView(savedInstanceState)
@@ -38,10 +60,59 @@ class EspTouch2Activity : BaseVmVbActivity<EspTouch2ViewModel, ActivityEsptouch2
         onCheckLocationPermission()
     }
 
-    private fun getWifiMessage(){
 
+    //---------------------    获取wifi信息     -------------------------------------------------------------------------------
+
+
+    /**
+     * 获取 wifi信息
+     */
+    private fun getCurrentWifiInfo() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && mNetworkCallback != null) {
+            mConnectivityManager.requestNetwork(mNetworkRequest, mNetworkCallback)
+        } else {
+            setWifiInfoData(mWifiManager.connectionInfo)
+        }
     }
 
+    /**
+     * 获取wifi信息
+     */
+    private fun setWifiInfoData(wifiInfo: WifiInfo) {
+        LogUtils.e("wifiInfo.ssid = ${wifiInfo.ssid}")
+        LogUtils.e("wifiInfo.bssid = ${wifiInfo.bssid}")
+        LogUtils.e("wifiInfo.ipAddress = ${wifiInfo.ipAddress}")
+    }
+
+    private val mNetworkCallback: ConnectivityManager.NetworkCallback? =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            object : ConnectivityManager.NetworkCallback(FLAG_INCLUDE_LOCATION_INFO) {
+                override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
+                    super.onCapabilitiesChanged(network, networkCapabilities)
+                    val wifiInfo = networkCapabilities.transportInfo as WifiInfo
+                    setWifiInfoData(wifiInfo)
+                }
+            }
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            object : ConnectivityManager.NetworkCallback() {
+                override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
+                    super.onCapabilitiesChanged(network, networkCapabilities)
+                    val wifiInfo = networkCapabilities.transportInfo as WifiInfo
+                    setWifiInfoData(wifiInfo)
+                }
+            }
+        } else {
+            null
+        }
+
+    fun unregisterNetWork() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            mConnectivityManager.bindProcessToNetwork(null)
+        }
+        if (mNetworkCallback != null) {
+            mConnectivityManager.unregisterNetworkCallback(mNetworkCallback)
+        }
+    }
 //---------------------    申请权限     -------------------------------------------------------------------------------
 
     /**
@@ -49,6 +120,7 @@ class EspTouch2Activity : BaseVmVbActivity<EspTouch2ViewModel, ActivityEsptouch2
      */
     private fun onLocationPermissionGranted() {
         showToastMessage("获取权限成功")
+        getCurrentWifiInfo()
     }
 
     /**
@@ -134,5 +206,10 @@ class EspTouch2Activity : BaseVmVbActivity<EspTouch2ViewModel, ActivityEsptouch2
             }, {
                 onLocationPermissionExplained()
             })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterNetWork()
     }
 }
