3f298aaeaebe68e0434944913c4bfa2ce607e5be
xue12311 <1712530035@qq.com>
2022-08-18 23:29:44 +0800

    获取 wifi信息

--- library_epager/src/main/java/com/ayxls/library_epager/bean/WiFiStateResultBean.kt
+++ library_epager/src/main/java/com/ayxls/library_epager/bean/WiFiStateResultBean.kt
@@ -19,4 +19,19 @@ data class WiFiStateResultBean(
     var wifi_bssid: String? = null,
     var wifi_rssi: Int = 0,
     var wifi_ssid_bytes: ByteArray? = null,
+
+    /**
+     * wifi 密码
+     */
+    var wifi_password: String? = null,
+
+    /**
+     * AES 密钥
+     */
+    var wifi_aes_key: String? = null,
+
+    /**
+     * 传递 自定义参数
+     */
+    var reserved_custom_data: ByteArray? = null,
 )
