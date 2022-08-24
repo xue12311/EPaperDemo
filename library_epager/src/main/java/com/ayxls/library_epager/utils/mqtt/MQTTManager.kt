cad56fafcdd9467e672fac20995a60dcec4f0d91
xue12311 <1712530035@qq.com>
2022-08-24 23:17:18 +0800

    mqtt配置

--- library_epager/src/main/java/com/ayxls/library_epager/utils/mqtt/MQTTManager.kt
+++ library_epager/src/main/java/com/ayxls/library_epager/utils/mqtt/MQTTManager.kt
@@ -1,7 +1,10 @@
 package com.ayxls.library_epager.utils.mqtt
 
+import org.eclipse.paho.client.mqttv3.MqttCallback
 import org.eclipse.paho.client.mqttv3.MqttClient
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions
+
+//https://blog.csdn.net/weixin_42324979/article/details/118030790
 
 class MQTTManager {
     private val TAG = MQTTManager::class.java.name
@@ -15,10 +18,12 @@ class MQTTManager {
      * 连接到MQTT服务器的 用户名
      */
     private var mqtt_service_username: String = ""
+
     /**
      * 连接到MQTT服务器的 密码
      */
     private var mqtt_service_password: String = ""
+
     /**
      * 连接到MQTT服务器的 客户端id
      */
@@ -32,12 +37,15 @@ class MQTTManager {
     /**
      * MQTT客户端
      */
-    private var mMqttClient:MqttClient? = null
+    private var mMqttClient: MqttClient? = null
 
     /**
      * MQTT连接选项
      */
     private var mMqttConnectOptions: MqttConnectOptions? = null
+
+    //消息回调
+    private val mCallback: MqttCallback = MyMqttCallback()
 
     companion object {
         private var mInstance: MQTTManager? = null
--- library_epager/src/main/java/com/ayxls/library_epager/utils/mqtt/MyMqttCallback.kt
+++ library_epager/src/main/java/com/ayxls/library_epager/utils/mqtt/MyMqttCallback.kt
@@ -1,10 +1,12 @@
 package com.ayxls.library_epager.utils.mqtt
 
+import com.blankj.utilcode.util.LogUtils
+import com.blankj.utilcode.util.StringUtils
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended
 import org.eclipse.paho.client.mqttv3.MqttMessage
 
-class MyMqttCallback:MqttCallbackExtended{
+class MyMqttCallback : MqttCallbackExtended {
     /**
      * 在连接已经连上且丢失后走这里
      *
@@ -20,6 +22,13 @@ class MyMqttCallback:MqttCallbackExtended{
      * @param message 实际信息内容
      */
     override fun messageArrived(topic: String?, message: MqttMessage?) {
+        if (topic == null || StringUtils.isTrimEmpty(topic) || message == null) {
+            return
+        }
+        val str_message = String(message.payload)
+        LogUtils.e("订阅 主题 Topic : $topic")
+        LogUtils.e("接受内容 message : $str_message")
+//        response(topic,1,"消息已收到")
     }
 
     /**
@@ -38,4 +47,22 @@ class MyMqttCallback:MqttCallbackExtended{
      */
     override fun connectComplete(reconnect: Boolean, serverURI: String?) {
     }
+
+    /**
+     *  响应
+     *  收到服务端户端的消息后，响应给对方告知消息已到达或者消息有问题等
+     * @param topic 主题
+     * @param qos 服务质量
+     * @param message 消息内容
+     */
+//    private fun onMqttResponse(topic: String, qos: Int, message: String) {
+//        //是否保留
+//        val isRetained = false
+//        try {
+//            //参数分别为：主题、消息的字节数组、服务质量、是否在服务器保留断开连接后的最后一条消息
+//            mqttAndroidClient.publish(topic, message.toByte(), qos, isRetained)
+//        } catch (e:MqttException) {
+//            e.printStackTrace()
+//        }
+//    }
 }
